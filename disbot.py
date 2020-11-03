c60474d86bb5ebff501928cba0f0d74c2d961bb6
barpacha <bar2012p@mail.ru>
2020-11-03 21:44:59 +0700

    '1.1'

--- disbot.py
+++ disbot.py
@@ -9,17 +9,13 @@ bot = discord.Client()
 @bot.event
 async def on_message(message):
     if message.content[0] == '$':
-        if message.author.voice is None:
+        if message.author.voice is None or not bot.voice_clients is None:
             return
         lg = 'russian' if (message.content[1] < 'a' and message.content[1] > 'z') else 'english'
         voices = tts.getProperty('voices')
         for voice in voices:
             if voice.name == lg:
                 tts.setProperty('voice', voice.id)
-
-
-
-
         voice = await message.author.voice.channel.connect(reconnect = True)
         if voice.is_connected():
             tts.save_to_file(message.content[1:], 'botsay.mp3')
@@ -28,4 +24,10 @@ async def on_message(message):
             while voice.is_playing():
                 await asyncio.sleep(1)
             await voice.disconnect()
+        await message.edit(content=None)
+    else:
+        if message.content == 'stop' and not bot.voice_clients is None:
+                voice = await message.author.voice.channel.connect(reconnect=True)
+                voice.stop()
+                await voice.disconnect()
 bot.run(TOKEN)
