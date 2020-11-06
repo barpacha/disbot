8ac16d0156b99ab9343c8ebada76a07a1b08c671
barpacha <bar2012p@mail.ru>
2020-11-06 19:45:43 +0700

    2.0

--- disbot.py
+++ disbot.py
@@ -1,9 +1,7 @@
-import pyttsx3
+import subprocess
 import discord
 import asyncio
 TOKEN = 'NzMzMDA1MDYyNDQ3ODI1MDA3.Xw82KQ.nXIeG_Xievu-d6lcKr6Bq5Spjjo'
-
-tts = pyttsx3.init()
 
 bot = discord.Client()
 @bot.event
@@ -11,24 +9,18 @@ async def on_message(message):
     if message.content[0] == '$':
         if message.author.voice is None:
             return
-        lg = 'russian' if (message.content[1] < 'a' and message.content[1] > 'z') else 'english'
-        voices = tts.getProperty('voices')
-        for voice in voices:
-            if voice.name == lg:
-                tts.setProperty('voice', voice.id)
         voice = await message.author.voice.channel.connect(reconnect = True)
         if voice.is_connected():
-            tts.save_to_file(message.content[1:], 'botsay.mp3')
-            tts.runAndWait()
+            subprocess.Popen(['balcon.exe', '-n', 'Microsoft Irina Desktop', '-t', message.content[1:], '-w','botsay.mp3']).wait()
             voice.play(discord.FFmpegPCMAudio(executable="C:/ffmpeg/bin/ffmpeg", source='botsay.mp3'), after=None)
             while voice.is_playing():
                 await asyncio.sleep(1)
             await voice.disconnect()
-            message.delete()
+            await message.delete()
     else:
         if message.content == 'stop' and not bot.voice_clients is None:
             voice = await message.author.voice.channel.connect(reconnect=True)
             voice.stop()
             await voice.disconnect()
-            message.delete()
+            await message.delete()
 bot.run(TOKEN)
